18e97f843f02bc2ceb40dc53f054dc7ff5b8b096
shakil-hasan <dev.shakilhasan22@gmail.com>
2024-07-17 03:14:34 +0600

    Second Commit

--- script.js
+++ script.js
@@ -1,3 +1,68 @@
-var a;
-a =10;
-console.log(a);
+//window.alert("Hello! I'm Window Alert");
+//console.log("Hello! I'm Console Log");
+
+/*var myName= prompt("Enter your name:");
+document.write("Hello " + myName);*/
+
+/*var a;
+a =30;
+b=a;
+document.write(b);*/
+
+//exponential
+/*var aNumber= prompt("Enter any number");
+var square= aNumber**2;
+document.write(square);*/
+
+//increment, decrement
+/*var a =12;
+a++;
+var b= 13;
+b--;
+document.write(a);
+console.log(b);*/
+
+//String Operator
+/* var line1="Hello";
+ var line2="World";
+ document.write(line1+" "+ line2);*/
+
+
+//Comparison Operator
+ /*var a=10;
+ var b=15;
+console.log(a==b);
+var c= 10;
+var d=12;
+console.log(c<d);
+var e=20;
+var f=20;
+document.write(e>f);*/
+
+//Logical Operator 
+/*||
+&&
+! */
+
+
+//Conditional Operator  ? :
+/* var a= 10>15 ? "Right" : "Wrong";
+document.write(a); */
+
+//A Project: Take a number from user (Temperature in celcius)
+//Convert it to Forenheit (F=9/5*C+32)
+//Print the result as alert
+//Print "Success"
+
+/*var C= prompt("Enter Temperature in Celcuis: ");
+ var F= 9/5*C+32;
+ window.alert("The Temperature in Farenheit is: " + F);
+ document.write("Success");*/
+
+ //Template Literals--Backtick
+/* var a = `Hello World! 
+    We are Learning JavaScript`;
+ console.log(a); */
+
+ 
+
